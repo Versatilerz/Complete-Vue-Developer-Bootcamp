55c19ccb9df4593352b9be7f4b19dd07f182bfe9
N. vG <nils.vangestel@gmail.com>
2024-06-27 22:47:26 +0200

    methods

--- 2.1/app.js
+++ 2.1/app.js
@@ -5,6 +5,11 @@ const vm = Vue.createApp({
       lastName: " van Gestel",
     };
   },
+  methods: {
+    fullName() {
+      return `${this.firstName} ${this.lastName.toUpperCase()}`;
+    },
+  },
 }).mount("#app");
 
 // setTimeout(() => {
