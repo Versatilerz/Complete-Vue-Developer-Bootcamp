a5f12a19f615d91064c35ab312f43c13709a4134
N. vG <nils.vangestel@gmail.com>
2024-06-27 14:43:46 +0200

    init project one

---
+++ ProjectOne/src/App.vue
@@ -0,0 +1,15 @@
+// first script without setup for the export default name property so debuggers
+work better, still needed? test
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<script setup></script>
+
+<template>
+  <div>Hello world in Vue!</div>
+</template>
+
+<style scoped></style>
