731cdc2295f6316401b24bd0da4628c3a6ec5561
N. vG <nils.vangestel@gmail.com>
2024-07-01 22:45:27 +0200

    mount

--- Section 4-1-Starter/app.js
+++ Section 4-1-Starter/app.js
@@ -1,7 +1,11 @@
 let vm = Vue.createApp({
   data() {
     return {
-      message: "Hello world!"
-    }
-  }
-}).mount('#app')
+      message: "Hello world!",
+    };
+  },
+}).mount("#app");
+
+// setTimeout(() => {
+//   vm.mount("#app");
+// }, 3000);
