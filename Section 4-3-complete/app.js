8148f6e9fdd8b128f681624fea205d2d8db618db
Nils van Gestel <nvangestel@yellowtail.nl>
2024-07-02 16:14:19 +0200

    templates

--- Section 4-3-complete/app.js
+++ Section 4-3-complete/app.js
@@ -4,30 +4,7 @@ let vm = Vue.createApp({
       message: "Hello world!",
     };
   },
-  beforeCreate() {
-    console.log("beforeCreate() function called!", this.message);
-  },
-  created() {
-    console.log("created() function called!", this.message);
-  },
-  beforeMount() {
-    console.log("beforeMount() function called!", this.$el);
-  },
-  mounted() {
-    console.log("mounted() function called!", this.$el);
-  },
-  beforeUpdate() {
-    console.log("beforeUpdate() function called!");
-  },
-  updated() {
-    console.log("updated() funcction called!");
-  },
-  beforeUnmount() {
-    console.log("beforeUnmount() function called!");
-  },
-  unmounted() {
-    console.log("unmounted() function called!");
-  },
+  template: `{{message}}`,
 });
 
 vm.mount("#app");
@@ -35,3 +12,28 @@ vm.mount("#app");
 // setTimeout(() => {
 //   vm.mount('#app')
 // }, 3000)
+
+// beforeCreate() {
+//   console.log("beforeCreate() function called!", this.message);
+// },
+// created() {
+//   console.log("created() function called!", this.message);
+// },
+// beforeMount() {
+//   console.log("beforeMount() function called!", this.$el);
+// },
+// mounted() {
+//   console.log("mounted() function called!", this.$el);
+// },
+// beforeUpdate() {
+//   console.log("beforeUpdate() function called!");
+// },
+// updated() {
+//   console.log("updated() funcction called!");
+// },
+// beforeUnmount() {
+//   console.log("beforeUnmount() function called!");
+// },
+// unmounted() {
+//   console.log("unmounted() function called!");
+// },
