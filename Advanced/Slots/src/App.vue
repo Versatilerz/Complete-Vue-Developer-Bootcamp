0eb89ba2e0c41e52817f59198c9bf3797e209a00
Nils van Gestel <nvangestel@yellowtail.nl>
2024-07-04 20:56:33 +0200

    initial setup slots example

---
+++ Advanced/Slots/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <form>
+    <div class="help"></div>
+    <div class="fields"></div>
+    <div class="buttons"></div>
+  </form>
+</template>
+<script>
+export default {
+  name: "App",
+};
+</script>
