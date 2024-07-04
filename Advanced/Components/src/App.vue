8e22d52d3b4fc858d18a4c22d51a648a0233c873
Nils van Gestel <nvangestel@yellowtail.nl>
2024-07-04 10:16:41 +0200

    test

--- Advanced/Components/src/App.vue
+++ Advanced/Components/src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <Greeting />
+  <p>
+    <Greeting />
+  </p>
 </template>
 
 <script>
--- Advanced/Components/src/components/Greeting.vue
+++ Advanced/Components/src/components/Greeting.vue
@@ -12,3 +12,9 @@ export default {
   },
 };
 </script>
+
+<style>
+p {
+  color: red;
+}
+</style>
