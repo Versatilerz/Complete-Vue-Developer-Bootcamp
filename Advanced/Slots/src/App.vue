ee2c70f7e433201fb28d3a3ef548db016ec1d564
Nils van Gestel <nvangestel@yellowtail.nl>
2024-07-04 21:06:24 +0200

    forms and slots

--- Advanced/Slots/src/App.vue
+++ Advanced/Slots/src/App.vue
@@ -1,12 +1,25 @@
 <template>
-  <form>
-    <div class="help"></div>
-    <div class="fields"></div>
-    <div class="buttons"></div>
-  </form>
+  <AppForm>    
+    <div><p>This is some help text</p></div>
+    <div>
+      <input class="help" placeholder="email"></input>
+      <input class="fields" placeholder="username"></input>
+      <input class="buttons" placeholder="password"></input>
+    </div>
+    <div class="buttons">
+      <button type="submit">Submit</button>
+    </div>
+    </AppForm>
+    <AppForm></AppForm>
 </template>
+
 <script>
+import AppForm from "./components/Form.vue";
+
 export default {
   name: "App",
+  components: {
+    AppForm,
+  },
 };
 </script>
