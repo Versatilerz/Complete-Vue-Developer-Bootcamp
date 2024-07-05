42c1be4f8961f09a574c858b5d571ccc4aa4fc88
N. vG <nils.vangestel@gmail.com>
2024-07-05 22:20:16 +0200

    add a form

--- Traversy_apps/Vue-Crash-2024-todo/src/App2.vue
+++ Traversy_apps/Vue-Crash-2024-todo/src/App2.vue
@@ -1,24 +1,26 @@
-<script>
-export default {
-  data() {
-    return {
-      name: "Composition API",
-      status: "pending",
-      tasks: ["Task one", "Task two", "Task three"],
-      link: "https://google.com",
-    };
-  },
-  methods: {
-    changeStatus() {
-      if (this.status === "active") {
-        this.status = "pending";
-      } else if (this.status === "pending") {
-        this.status = "inactive";
-      } else {
-        this.status = "active";
-      }
-    },
-  },
+<script setup>
+import { ref } from "vue";
+
+const name = ref("Composition API");
+const status = ref("active");
+const tasks = ref(["Task one", "Task two", "Task three"]);
+const newTask = ref("");
+
+const changeStatus = () => {
+  if (status.value === "active") {
+    status.value = "pending";
+  } else if (status.value === "pending") {
+    status.value = "inactive";
+  } else {
+    status.value = "active";
+  }
+};
+
+const addTask = () => {
+  if (newTask.value.trim() !== "") {
+    tasks.value.push(newTask.value);
+    newTask.value = "";
+  }
 };
 </script>
 
@@ -28,13 +30,20 @@ export default {
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
 
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask" />
+
+    <button type="submit">Submit</button>
+  </form>
+
   <h3>Tasks:</h3>
   <ul>
     <li v-for="(task, index) in tasks">{{ task }}</li>
   </ul>
 
   <!-- <a v-bind:href="link">Click for Google</a> -->
-  <a :href="link">Click for Google</a>
+  <!-- <a :href="link">Click for Google</a> -->
   <br />
   <!-- <button v-on:click="changeStatus">Change Status</button> -->
   <button @click="changeStatus">Change Status</button>
