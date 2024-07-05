d58b6b3bb114bf78c35d1f0af47cc2e2914535e4
Nils van Gestel <nvangestel@yellowtail.nl>
2024-07-05 16:12:45 +0200

    switching between components

--- Quiz/src/App.vue
+++ Quiz/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="ctr">
-    <Questions />
-    <Result />
+    <Questions v-if="questionsAnswered < questions.length" />
+    <Result v-else />
     <button type="button" class="reset-btn">Reset</button>
   </div>
 </template>
@@ -18,6 +18,7 @@ export default {
   },
   data() {
     return {
+      questionsAnswered: 0,
       questions: [
         {
           q: "What is 2 + 2?",
