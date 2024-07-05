837bef129052b7acd74de2826824508b32bb56dc
Nils van Gestel <nvangestel@yellowtail.nl>
2024-07-05 12:43:53 +0200

    .

--- Advanced/Animations/src/App.vue
+++ Advanced/Animations/src/App.vue
@@ -5,8 +5,21 @@
     <h2 v-else key="secondary">Another Hello!</h2>
   </transition> -->
 
-  <transition name="zoom" appear>
+  <!-- <transition name="zoom" appear>
     <h2 v-if="flag">Hello</h2>
+  </transition> -->
+
+  <transition
+    @before-enter="beforeEnter"
+    @enter="enter"
+    @after-enter="afterEnter"
+    @before-leave="beforeLeave"
+    @leave="leave"
+    @after-leave="afterLeave"
+    :css="true"
+    name="fade"
+  >
+    <h2 v-if="flag">Hey</h2>
   </transition>
 </template>
 
@@ -17,6 +30,36 @@ export default {
     return {
       flag: true,
     };
+  },
+  methods: {
+    beforeEnter(el) {
+      console.log("before-enter event fired", el);
+    },
+    enter(el) {
+      console.log("enter event fired", el);
+      // const animation = el.animate([{ transform: "scale3d(0,0,0)" }, {}], 1000);
+
+      // animation.onfinish = () => {
+      //   done();
+      // };
+    },
+    afterEnter(el) {
+      console.log("after-enter event fired", el);
+    },
+    beforeLeave(el) {
+      console.log("before-leave event fired", el);
+    },
+    leave(el) {
+      console.log("leave event fired", el);
+      // const animation = el.animate([{}, { transform: "scale3d(0,0,0)" }], 1000);
+
+      // animation.onfinish = () => {
+      //   done();
+      // };
+    },
+    afterLeave(el) {
+      console.log("after-leave event fired", el);
+    },
   },
 };
 </script>
