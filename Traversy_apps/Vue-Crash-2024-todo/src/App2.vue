1b18320a923c9cfd8d284c4bf70ccc47fb398454
N. vG <nils.vangestel@gmail.com>
2024-07-05 22:12:55 +0200

    options API done, setup composition API

---
+++ Traversy_apps/Vue-Crash-2024-todo/src/App2.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "Composition API",
+      status: "pending",
+      tasks: ["Task one", "Task two", "Task three"],
+      link: "https://google.com",
+    };
+  },
+  methods: {
+    changeStatus() {
+      if (this.status === "active") {
+        this.status = "pending";
+      } else if (this.status === "pending") {
+        this.status = "inactive";
+      } else {
+        this.status = "active";
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="(task, index) in tasks">{{ task }}</li>
+  </ul>
+
+  <!-- <a v-bind:href="link">Click for Google</a> -->
+  <a :href="link">Click for Google</a>
+  <br />
+  <!-- <button v-on:click="changeStatus">Change Status</button> -->
+  <button @click="changeStatus">Change Status</button>
+</template>
--- Traversy_apps/Vue-Crash-2024-todo/src/main.js
+++ Traversy_apps/Vue-Crash-2024-todo/src/main.js
@@ -1,6 +1,8 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App1 from "./App1.vue";
+import App2 from "./App2.vue";
 
-createApp(App).mount('#app')
+createApp(App1).mount("#app1");
+createApp(App2).mount("#app2");
