8f8d5ab8b49bdd36010f3a7a820b9cc0bc793a56
Nils van Gestel <nvangestel@yellowtail.nl>
2024-07-07 14:44:27 +0200

    api replace

--- Traversy_apps/Vue-Crash-2024-todo/src/components/BackButton.vue
+++ Traversy_apps/Vue-Crash-2024-todo/src/components/BackButton.vue
@@ -6,10 +6,10 @@ import { RouterLink } from "vue-router";
   <section>
     <div class="container m-auto py-6 px-6">
       <RouterLink
-        href="jobs.html"
+        to="/jobs"
         class="text-green-500 hover:text-green-600 flex items-center"
       >
-        <i class="fas fa-arrow-left mr-2"></i> Back to Job Listings
+        <i class="pi pi-arrow-circle-left mr-3"></i> Back to Job Listings
       </RouterLink>
     </div>
   </section>
