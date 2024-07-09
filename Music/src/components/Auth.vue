62d7b129a3cd5ec26313ea0e911da0a68daf9315
Nils van Gestel <nvangestel@yellowtail.nl>
2024-07-09 16:02:30 +0200

    toggle

--- Music/src/components/Auth.vue
+++ Music/src/components/Auth.vue
@@ -4,9 +4,16 @@ import { mapState, mapWritableState } from 'pinia';
 
 export default {
   name: 'Auth',
+  data() {
+    return {
+      tab: 'login'
+    };
+  },
   computed: {
     ...mapState(useModalStore, ['hiddenClass']),
-    ...mapWritableState(useModalStore, ['isOpen'])
+    ...mapWritableState(useModalStore, {
+      modalVisibility: 'isOpen'
+    })
   }
 };
 </script>
@@ -30,7 +37,7 @@ export default {
         <!-- Add margin if you want to see some of the overlay behind the modal-->
         <div class="py-4 text-left px-6">
           <!--Title-->
-          <div class="flex justify-between items-center pb-4" @click="isOpen = false">
+          <div class="flex justify-between items-center pb-4" @click="modalVisibility = false">
             <p class="text-2xl font-bold">Your Account</p>
             <!-- Modal Close Button -->
             <div class="modal-close cursor-pointer z-50">
@@ -42,18 +49,32 @@ export default {
           <ul class="flex flex-wrap mb-4">
             <li class="flex-auto text-center">
               <a
-                class="block rounded py-3 px-4 transition hover:text-white text-white bg-blue-600"
+                @click.prevent="tab = 'login'"
+                class="block rounded py-3 px-4 transition"
+                :class="{
+                  'hover:text-white text-white bg-blue-600': tab === 'login',
+                  'hover:text-blue-600': tab === 'register'
+                }"
                 href="#"
                 >Login</a
               >
             </li>
             <li class="flex-auto text-center">
-              <a class="block rounded py-3 px-4 transition" href="#">Register</a>
+              <a
+                @click.prevent="tab = 'register'"
+                class="block rounded py-3 px-4 transition"
+                :class="{
+                  'hover:text-white text-white bg-blue-600': tab === 'register',
+                  'hover:text-blue-600': tab === 'login'
+                }"
+                href="#"
+                >Register</a
+              >
             </li>
           </ul>
 
           <!-- Login Form -->
-          <form>
+          <form v-show="tab === 'login'">
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
@@ -80,7 +101,7 @@ export default {
             </button>
           </form>
           <!-- Registration Form -->
-          <form>
+          <form v-show="tab === 'register'">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
