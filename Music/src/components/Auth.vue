9572ee3b3d01a15906cc4875d59c0aeca6a92b71
Nils van Gestel <nvangestel@yellowtail.nl>
2024-07-09 17:00:55 +0200

    first error message and validation

--- Music/src/components/Auth.vue
+++ Music/src/components/Auth.vue
@@ -6,7 +6,16 @@ export default {
   name: 'Auth',
   data() {
     return {
-      tab: 'login'
+      tab: 'login',
+      schema: {
+        Name: 'required'
+        // Email: ' ',
+        // Age: ' ',
+        // Password: '',
+        // Confirm_password: '',
+        // Countr: '',
+        // Tos: ''
+      }
     };
   },
   computed: {
@@ -101,15 +110,17 @@ export default {
             </button>
           </form>
           <!-- Registration Form -->
-          <form v-show="tab === 'register'">
+          <vee-form v-show="tab === 'register'" :validation-schema="schema">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
-              <input
+              <vee-field
+                name="Name"
                 type="text"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Name"
               />
+              <ErrorMessage class="text-red-600" name="Name" />
             </div>
             <!-- Email -->
             <div class="mb-3">
@@ -168,7 +179,7 @@ export default {
             >
               Submit
             </button>
-          </form>
+          </vee-form>
         </div>
       </div>
     </div>
