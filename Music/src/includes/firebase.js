df6626e526daa6629c3f50d179b6a91c6c6f22a4
Nils van Gestel <nvangestel@yellowtail.nl>
2024-07-12 15:21:26 +0200

    setup firebase

--- Music/src/includes/firebase.js
+++ Music/src/includes/firebase.js
@@ -1,1 +1,13 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDHZH_iOOcxavyrsj7YHSwcpXQ6bv4okOY',
+  authDomain: 'music-vue-15343.firebaseapp.com',
+  projectId: 'music-vue-15343',
+  storageBucket: 'music-vue-15343.appspot.com',
+  appId: '1:739620497838:web:587f96ab91275e13eb403f'
+  //   measurementId: 'G-QT6ZTW9XF0'
+};
+
+export default firebase.initializeApp(firebaseConfig);
